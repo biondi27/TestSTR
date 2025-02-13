416d66b01e74ce84c6ea045a6572372db5ec80c3
biondi27 <122488988+biondi27@users.noreply.github.com>
2025-02-12 21:27:00 -0400

    Update Controlador.py

--- Controlador.py
+++ Controlador.py
@@ -7,13 +7,13 @@ usuario = 'HVAC_Lorenzo' # input('Ingrese su Usuario: ')
 client = redis.StrictRedis(host='[DIRECCIÓN IP]',port='[PUERTO]',db=0, decode_responses=True)
 
 Temperatura = 30
-Objetivo = 10 #añadir temp objetivo o ajustar manual?
+Objetivo = 15
 
 def listen_redis():
     try:
         pubsub = client.pubsub()
-        pubsub.subscribe('joerlyn')
-        print("Sistema HVAC en línea.")
+        pubsub.subscribe('_____')
+        client.publish(CHANNEL,f'{usuario}: Sistema HVAC en línea.')
         for message in pubsub.listen(): # Capturar mensajes
             if message['type'] == 'message':
                 message_converter(message) # Enviar mensajes al convertidor
@@ -23,62 +23,68 @@ def listen_redis():
 
 def message_converter(message):
     global Temperatura
+    global Objetivo
     try:
         num = int(message['data'])
         Temperatura = num
-        message = f"La temperatura es : {Temperatura} Grados Celcius."
+        message = f"La temperatura ha cambiado a {Temperatura} Grados Celcius."
         client.publish(CHANNEL,f'{usuario}: {message}')
-        if Temperatura > 18:
+        if Temperatura > Objetivo:
             message = "Se ha detectado una temperatura elevada. Enfriando..."
             client.publish(CHANNEL,f'{usuario}: {message}')
-        elif Temperatura < 18:
+        elif Temperatura < Objetivo:
             message = "Se ha detectado una temperatura reducida. Calentando..."
             client.publish(CHANNEL,f'{usuario}: {message}')
 
     except ValueError:
         message = "El mensaje no es una temperatura." 
-        client.publish(CHANNEL, f'{usuario}: {message}') # Comentar si repite mucho el mensaje
+        # client.publish(CHANNEL, f'{usuario}: {message}') # Comentar si repite mucho el mensaje
 
 listen_redis()
 
 # Reguladores
 def AC():
     global Temperatura
-    while Temperatura != 18:
-        if Temperatura < 18:
+    global Objetivo
+    while Temperatura != Objetivo:
+        if Temperatura < Objetivo:
             Temperatura += 5
-        elif Temperatura > 18:
+        elif Temperatura > Objetivo:
             Temperatura -= 5
     time.sleep(3)
 
 def Vent():
     global Temperatura
-    while Temperatura != 18:
-        if Temperatura < 18:
+    global Objetivo
+    while Temperatura != Objetivo:
+        if Temperatura < Objetivo:
             Temperatura += 3
-        elif Temperatura > 18:
+        elif Temperatura > Objetivo:
             Temperatura -= 3
     time.sleep(2)
 
 def Pasivo():
     global Temperatura
-    while Temperatura != 18:
-        if Temperatura < 18:
+    global Objetivo
+    while Temperatura != Objetivo:
+        if Temperatura < Objetivo:
             Temperatura += 1
-        elif Temperatura > 18:
+        elif Temperatura > Objetivo:
             Temperatura -= 1
     time.sleep(1)
 
 def Reporte():
     global Temperatura
-    while Temperatura != 18:
+    global Objetivo
+    while Temperatura != Objetivo:
         message = f"El cuarto está a {Temperatura} Grados Celcius."
         client.publish(CHANNEL,f'{usuario}: {message}')
     time.sleep(3)
 
 def Estable():
     global Temperatura
-    while Temperatura == 18:
+    global Objetivo
+    while Temperatura == Objetivo:
         message = f"Temperatura estable a {Temperatura} Grados Celcius."
         client.publish(CHANNEL,f'{usuario}: {message}')
     time.sleep(30)
